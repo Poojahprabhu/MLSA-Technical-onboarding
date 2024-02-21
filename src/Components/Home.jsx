c3458ef3328d61f38033727eef37deeadb54f193
Pooja H P <poojahprabhu18@gmail.com>
2024-02-22 01:45:24 +0530

    commit profile picture

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/me.png";
+import image from "../images/Pooja.png";
 
 const imageAltText = "profile image of Pooja H P";
 
